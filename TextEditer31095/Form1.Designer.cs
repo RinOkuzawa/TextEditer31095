7d5f2c6c0a8338214e253b999a70941db037c482
RinOkuzawa <31095@ojs.ac.jp>
2020-08-20 12:00:57 +0900

    開くと名前を付けて保存

--- TextEditer31095/Form1.Designer.cs
+++ TextEditer31095/Form1.Designer.cs
@@ -23,13 +23,156 @@
         /// コード エディターで変更しないでください。
         /// </summary>
         private void InitializeComponent() {
-            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.FileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.NewToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.OpenToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.updateSave保存ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.NameSaveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.ExitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.編集EToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.文字コードCToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ヘルプHToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ofdFileOpen = new System.Windows.Forms.OpenFileDialog();
+            this.sfdFileSave = new System.Windows.Forms.SaveFileDialog();
+            this.rtbTextArea = new System.Windows.Forms.RichTextBox();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.FileToolStripMenuItem,
+            this.編集EToolStripMenuItem,
+            this.文字コードCToolStripMenuItem,
+            this.ヘルプHToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(896, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // FileToolStripMenuItem
+            // 
+            this.FileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.NewToolStripMenuItem,
+            this.OpenToolStripMenuItem,
+            this.updateSave保存ToolStripMenuItem,
+            this.NameSaveToolStripMenuItem,
+            this.toolStripSeparator1,
+            this.ExitToolStripMenuItem});
+            this.FileToolStripMenuItem.Name = "FileToolStripMenuItem";
+            this.FileToolStripMenuItem.Size = new System.Drawing.Size(67, 20);
+            this.FileToolStripMenuItem.Text = "ファイル(&F)";
+            // 
+            // NewToolStripMenuItem
+            // 
+            this.NewToolStripMenuItem.Name = "NewToolStripMenuItem";
+            this.NewToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.N)));
+            this.NewToolStripMenuItem.Size = new System.Drawing.Size(184, 22);
+            this.NewToolStripMenuItem.Text = "新規作成(&N)";
+            // 
+            // OpenToolStripMenuItem
+            // 
+            this.OpenToolStripMenuItem.Name = "OpenToolStripMenuItem";
+            this.OpenToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.O)));
+            this.OpenToolStripMenuItem.Size = new System.Drawing.Size(184, 22);
+            this.OpenToolStripMenuItem.Text = "開く(&O)";
+            this.OpenToolStripMenuItem.Click += new System.EventHandler(this.OpenToolStripMenuItem_Click);
+            // 
+            // updateSave保存ToolStripMenuItem
+            // 
+            this.updateSave保存ToolStripMenuItem.Name = "updateSave保存ToolStripMenuItem";
+            this.updateSave保存ToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.S)));
+            this.updateSave保存ToolStripMenuItem.Size = new System.Drawing.Size(184, 22);
+            this.updateSave保存ToolStripMenuItem.Text = "上書き保存(&S)";
+            // 
+            // NameSaveToolStripMenuItem
+            // 
+            this.NameSaveToolStripMenuItem.Name = "NameSaveToolStripMenuItem";
+            this.NameSaveToolStripMenuItem.Size = new System.Drawing.Size(184, 22);
+            this.NameSaveToolStripMenuItem.Text = "名前を付けて保存(&A)";
+            this.NameSaveToolStripMenuItem.Click += new System.EventHandler(this.NameSaveToolStripMenuItem_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(181, 6);
+            // 
+            // ExitToolStripMenuItem
+            // 
+            this.ExitToolStripMenuItem.Name = "ExitToolStripMenuItem";
+            this.ExitToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Alt | System.Windows.Forms.Keys.F4)));
+            this.ExitToolStripMenuItem.Size = new System.Drawing.Size(184, 22);
+            this.ExitToolStripMenuItem.Text = "終了(&X)";
+            this.ExitToolStripMenuItem.Click += new System.EventHandler(this.ExitToolStripMenuItem_Click);
+            // 
+            // 編集EToolStripMenuItem
+            // 
+            this.編集EToolStripMenuItem.Name = "編集EToolStripMenuItem";
+            this.編集EToolStripMenuItem.Size = new System.Drawing.Size(57, 20);
+            this.編集EToolStripMenuItem.Text = "編集(&E)";
+            // 
+            // 文字コードCToolStripMenuItem
+            // 
+            this.文字コードCToolStripMenuItem.Name = "文字コードCToolStripMenuItem";
+            this.文字コードCToolStripMenuItem.Size = new System.Drawing.Size(82, 20);
+            this.文字コードCToolStripMenuItem.Text = "文字コード(&C)";
+            // 
+            // ヘルプHToolStripMenuItem
+            // 
+            this.ヘルプHToolStripMenuItem.Name = "ヘルプHToolStripMenuItem";
+            this.ヘルプHToolStripMenuItem.Size = new System.Drawing.Size(65, 20);
+            this.ヘルプHToolStripMenuItem.Text = "ヘルプ(&H)";
+            // 
+            // ofdFileOpen
+            // 
+            this.ofdFileOpen.FileName = "openFileDialog1";
+            // 
+            // rtbTextArea
+            // 
+            this.rtbTextArea.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.rtbTextArea.Location = new System.Drawing.Point(0, 24);
+            this.rtbTextArea.Name = "rtbTextArea";
+            this.rtbTextArea.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.ForcedHorizontal;
+            this.rtbTextArea.Size = new System.Drawing.Size(896, 553);
+            this.rtbTextArea.TabIndex = 1;
+            this.rtbTextArea.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(896, 577);
+            this.Controls.Add(this.rtbTextArea);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "テキストエディタ";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem FileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem NewToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem OpenToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem updateSave保存ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem NameSaveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem ExitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 編集EToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 文字コードCToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ヘルプHToolStripMenuItem;
+        private System.Windows.Forms.OpenFileDialog ofdFileOpen;
+        private System.Windows.Forms.SaveFileDialog sfdFileSave;
+        private System.Windows.Forms.RichTextBox rtbTextArea;
     }
 }
 
--- TextEditer31095/Form1.cs
+++ TextEditer31095/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,37 @@ namespace TextEditer31095 {
     public partial class Form1 : Form {
         public Form1() {
             InitializeComponent();
+        }
+        //アプリ終了
+        private void ExitToolStripMenuItem_Click(object sender, EventArgs e) {
+            Application.Exit();
+        }
+        //[開く]ダイアログを表示
+        private void OpenToolStripMenuItem_Click(object sender, EventArgs e) {
+
+
+            if(ofdFileOpen.ShowDialog() == DialogResult.OK) {
+                using (StreamReader sr = new StreamReader(ofdFileOpen.FileName,Encoding.GetEncoding("utf-8"),false)) {
+                    rtbTextArea.Text = sr.ReadToEnd();
+                   
+                    //string line
+                    //while ((line = sr.ReadLine()) != null) {
+                    //    rtbTextArea.Text = line;
+                    //}
+                    
+                }
+            
+            
+            }
+        }
+        //[名前を付けて保存]ダイアログを表示
+        private void NameSaveToolStripMenuItem_Click(object sender, EventArgs e) {
+            if(sfdFileSave.ShowDialog() == DialogResult.OK) {
+                using (StreamWriter sw = new StreamWriter(sfdFileSave.FileName, false,Encoding.GetEncoding("utf-8"))) {
+                    sw.WriteLine(rtbTextArea.Text);
+                }
+                   
+            }
         }
     }
 }
